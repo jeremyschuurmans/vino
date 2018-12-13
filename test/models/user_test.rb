78b0893303dc5c832d5096e8b022f75fffbeb80c
jeremy schuurmans <jeremy.shurminz@gmail.com>
2018-12-12 20:36:54 -0800

    add tests and validations for name length and email length

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -14,4 +14,19 @@ class UserTest < ActiveSupport::TestCase
     @user.name = " "
     assert_not @user.valid?
   end
+
+  test "email should be present" do
+    @user.email = " "
+    assert_not @user.valid?
+  end
+
+  test "name should not be longer than 100 characters" do
+    @user.name = "a" * 101
+    assert_not @user.valid?
+  end
+
+  test "email should not be longer than 100 characters" do
+    @user.email = "a" * 101 + "@hogwarts.edu"
+    assert_not @user.valid?
+  end
 end
