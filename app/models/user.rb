172dd4d49c8f3fd91c8baca1c7525a278709f3bf
jeremy schuurmans <jeremy.shurminz@gmail.com>
2018-12-12 21:33:57 -0800

    downcase all emails when saving

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  before_save { email.downcase! }
   validates :name, presence: :true, length: {maximum: 100}
-  validates :email, presence: :true, length: {maximum: 100}, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: { case_sensitive: false }
+  validates :email, presence: :true,
+                    length: {maximum: 100},
+                    format: { with: URI::MailTo::EMAIL_REGEXP },
+                    uniqueness: { case_sensitive: false }
 end
