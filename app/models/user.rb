a4e17a730c3553c90585e9bf074923d7c9caa161
jeremy schuurmans <jeremy.shurminz@gmail.com>
2018-12-12 21:22:09 -0800

    add tests and validations for email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
   validates :name, presence: :true, length: {maximum: 100}
-  validates :email, presence: :true, length: {maximum: 100}, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, presence: :true, length: {maximum: 100}, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: { case_sensitive: false }
 end
