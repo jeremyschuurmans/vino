e141a01fb01ddfb2a8442e4dcda5a2b475029b9f
jeremy schuurmans <jeremy.shurminz@gmail.com>
2018-12-13 13:34:37 -0800

    finish user sign up, add flash message

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   get '/new_wine', to: 'wines#new'
   resources :users
 end
