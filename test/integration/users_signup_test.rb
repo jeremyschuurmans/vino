5a187b557765d63d50d4f91c73e80b886d55638f
jeremy schuurmans <jeremy.shurminz@gmail.com>
2018-12-13 13:39:24 -0800

    add integration tests for user sign up

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -9,11 +9,25 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       post users_path, params: { user: { name: "",
                                          email: "harry@hogwarts",
                                          password: "hat",
-                                         password_confirmation: "tah"} }
+                                         password_confirmation: "tah" } }
 
     end
     assert_template 'users/new'
     assert_select 'div#error_message'
     assert_select 'div.field_with_errors'
+  end
+
+  test 'valid sign up' do
+    get signup_path
+
+    assert_difference 'User.count' do
+      post users_path, params: { user: { name: "Harry Potter",
+                                         email: "hpotter@hogwarts.edu",
+                                         password: "quidditch",
+                                         password_confirmation: "quidditch" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
   end
 end
