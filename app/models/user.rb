67c70855ea88c214071e9f987d75a7949e117261
jeremy schuurmans <jeremy.shurminz@gmail.com>
2018-12-12 21:00:38 -0800

    add tests and validations for email format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
   validates :name, presence: :true, length: {maximum: 100}
-  validates :email, presence: :true, length: {maximum: 100}
+  validates :email, presence: :true, length: {maximum: 100}, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -29,4 +29,20 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "a" * 101 + "@hogwarts.edu"
     assert_not @user.valid?
   end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[hpotter@pottermore.com hpotter@hogwarts.edu HARRY@potter.COM H_AR-RY@team.gryffindor.org harry.potter@hogwarts.jp harry+potter@hogwarts.co]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be a valid email"
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[harry@hogwarts,com harry_at_hogwarts.edu harry.potter@hogwarts. harry@hog_warts.com harry@hog+warts.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should not be valid"
+    end
+  end
 end
