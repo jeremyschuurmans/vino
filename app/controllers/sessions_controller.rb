7f7e5c2f1035fc8519212de38c4755c22386b32f
jeremy schuurmans <jeremy.shurminz@gmail.com>
2018-12-13 17:04:03 -0800

    add SessionsHelper module, current_user methods, etc.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,17 @@
 class SessionsController < ApplicationController
+  layout "alternate"
+
   def new
-    render :layout => 'alternate'
   end
 
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
-
+      log_in @user
+      redirect_to @user
     else
-      flash[:danger] = "Email or password not valid"
-      render 'new', :layout => 'alternate'
+      flash.now[:danger] = "Email or password not valid"
+      render 'new'
     end
   end
 
