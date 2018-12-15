3b44a059e8d213dfea47cc4cdcdd5726ce72f865
jeremy schuurmans <jeremy.shurminz@gmail.com>
2018-12-15 08:36:14 -0800

    complete log in and log out

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,7 +6,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = User.create(name: "Harry Potter", email: "hpotter@hogwarts.edu", password: "quidditch", password_confirmation: "quidditch")
   end
 
-  test "login with invalid username and password" do
+  test "log in with invalid username and password" do
     get login_path
     assert_template 'sessions/new'
     post login_path, params: { session: { email: " ", password: " " } }
@@ -16,15 +16,23 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
-  test "login with valid username and password" do
+  test "log in with valid username and password and then log out" do
     get login_path
     post login_path, params: { session: { email: "hpotter@hogwarts.edu",
                                           password: 'quidditch' } }
+    assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 end
