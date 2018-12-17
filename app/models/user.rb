35ac6d46088c1390f7f16f6c67655556586d1e1d
jeremy schuurmans <jeremy.shurminz@gmail.com>
2018-12-17 14:09:33 -0800

    when a user is deleted, the wine associated with the user is also deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :wines
+  has_many :wines, dependent: :destroy
   attr_accessor :remember_token
   before_save { self.email = email.downcase }
   validates :name, presence: :true, length: { maximum: 100 }
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -73,4 +73,18 @@ class UserTest < ActiveSupport::TestCase
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?('')
   end
+
+  test "when a user is deleted, the associated wines should also be deleted" do
+    @user.wines.create!(name: "Charles & Charles Rose",
+                        winery: "Charles & Charles",
+                        vintage: "2017",
+                        origin: "Columbia Valley, Washington",
+                        price: "$12",
+                        rating: 9,
+                        tasting_notes: "This wine is a pretty, pale-salmon color. Aromas of strawberry bubblegum, herb, tropical fruit and citrus peel lead to dry fruit flavors, full of papaya, guava and pink-grapefruit notes with a tart finish. It flat-out delivers.")
+    assert_difference 'Wine.count', -1 do
+      @user.destroy
+    end
+
+  end
 end
