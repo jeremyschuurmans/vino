9f4579b50858155e04938f00bbb2e219a4c73da5
jeremy schuurmans <jeremy.shurminz@gmail.com>
2018-12-18 13:10:57 -0800

    add preliminary code to new, edit, and update actions for wines

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,15 +1,27 @@
 class WinesController < ApplicationController
 
   def new
+    @wine = Wine.new
+    render :layout => 'alternate'
   end
 
   def create
   end
 
   def edit
+    @wine = Wine.find(params[:id])
+    render :layout => 'alternate'
   end
 
   def update
+    @user = current_user
+    @wine = Wine.find(params[:id])
+    if @wine.update_attributes(wine_params)
+      flash[:success] = "Wine successfully updated!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def destroy
