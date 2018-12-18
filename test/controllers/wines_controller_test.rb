3ced732f76ab8495b9d418bd336ad9afae95977c
jeremy schuurmans <jeremy.shurminz@gmail.com>
2018-12-18 13:21:02 -0800

    add wines controller test

--- test/controllers/wines_controller_test.rb
+++ test/controllers/wines_controller_test.rb
@@ -1,9 +1,23 @@
 require 'test_helper'
 
 class WinesControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get new_wine_path
-    assert_response :success
+
+  def setup
+    @wine = wines(:rose)
   end
+
+  test "should not allow users to access create when not logged in" do
+    assert_no_difference 'Wine.count' do
+      post wines_path, params: { wine: { name: "Rombauer Chardonnay",
+                                         winery: "Rombauer Vineyards",
+                                         vintage: "2017",
+                                         origin: "Carneros, California",
+                                         price: "$32",
+                                         rating: 10,
+                                         tasting_notes: "The 2017 Chardonnay features compelling spiced apples, almond croissant, honeyed peaches and fresh baked pear tart notions on the nose. Medium-bodied with a satiny texture, the palate delivers plenty of peaches and savory layers with a racy line and mineral-tinged finish." } }
+
+       end
+       assert_redirected_to login_url
+    end
 
 end
