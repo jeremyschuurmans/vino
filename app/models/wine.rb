a87e284736f3ba66f926af5b2359dfb081be3c54
jeremy schuurmans <jeremy.shurminz@gmail.com>
2018-12-19 12:27:53 -0800

    add image uploading and processing

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -10,4 +10,13 @@ class Wine < ApplicationRecord
             :price,
             :rating,
             :tasting_notes, presence: true
+  validate :picture_size
+
+  private
+
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "file should be less than 5MB")
+      end
+    end
 end
