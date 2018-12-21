5df8fdad67a65847d422a26aba7df00a6cb06683
jeremy schuurmans <jeremy.shurminz@gmail.com>
2018-12-20 20:36:15 -0800

    create routes for creating and deleting relationships, create follow/unfollow button, render on user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get '/about', to: 'static_pages#about'
-  get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  root                   'static_pages#home'
+  get    '/about',   to: 'static_pages#about'
+  get    '/signup',  to: 'users#new'
+  post   '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users do
     member do
       get :following, :followers
     end
   end
-  resources :wines, only: [:new, :create, :edit, :update, :destroy]
+  resources :wines,         only: [:new, :create, :edit, :update, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
