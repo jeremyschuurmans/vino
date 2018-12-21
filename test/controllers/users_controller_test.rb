83a6583dfb0af1a8ecdc218c732f38ebbbb51aad
jeremy schuurmans <jeremy.shurminz@gmail.com>
2018-12-20 21:00:32 -0800

    add tests for viewing followers/following when not logged in, refine profile page styling

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -69,4 +69,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to root_url
   end
+
+  test "should not allow user to view following when not logged in" do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
+
+  test "should not allow users to view followers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
+  end
 end
