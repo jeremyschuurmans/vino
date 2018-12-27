428c79c4dd601a13f7bd376689df9da1e373fd9a
jeremy schuurmans <jeremy.shurminz@gmail.com>
2018-12-27 12:36:02 -0800

    start testing facebook login

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get login_path
-    assert_response :success
+
+  def setup
+    request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:facebook]
+  end
+
+  test "should successfully create a user" do
+    post :create, provider: :facebook
+    assert is_logged_in?
   end
 
 end
