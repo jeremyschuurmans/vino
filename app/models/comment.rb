63d7226f0a0b462421e2e0b4cb28a31dfc63c0b0
jeremy schuurmans <jeremy.shurminz@gmail.com>
2018-12-29 12:43:41 -0800

    add validations to comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   belongs_to :user
   has_many :comments, as: :commentable
+  validates :body, presence: :true
+  validates :user_id, presence: :true
 end
