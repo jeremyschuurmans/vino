6de44b44bd5261070a7961048ed40bbd84690eb5
Jeremy Schuurmans <schuurm@ns.codes>
2019-01-29 17:01:23 -0800

    configure facebook omniauth for production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  if rails.env.development?
+    provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  else
+    provider :facebook, ENV['FACEBOOK_KEY_PRODUCTION'], ENV['FACEBOOK_SECRET_PRODUCTION']
+  end
 end
