ebc7736dc8fb192d314dca4efb4ce230cdb2cd74
Jeremy Schuurmans <schuurm@ns.codes>
2019-08-28 12:07:49 -0700

    fix other corrupted files

--- config/environment.rb
+++ config/environment.rb
@@ -1,21 +1,5 @@
-<<<<<<< HEAD
-ENV['SINATRA_ENV'] ||= "development"
-ENV['RACK_ENV'] ||= "development"
-
-require 'bundler/setup'
-Bundler.require(:default, ENV['SINATRA_ENV'])
-
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
-
-require './app/controllers/application_controller'
-require_all 'app'
-=======
 # Load the Rails application.
 require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
->>>>>>> 838f433e9496433f5afd2ec9171af631f6972d9a
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root                                 'static_pages#home'
   get    '/signup',                to: 'users#new'
   post   '/signup',                to: 'users#create'
-  get    '/auth/github/callback', to: 'sessions#create'
+  get    '/auth/github/callback',  to: 'sessions#create'
   get    '/login',                 to: 'sessions#new'
   post   '/login',                 to: 'sessions#create'
   delete '/logout',                to: 'sessions#destroy'
