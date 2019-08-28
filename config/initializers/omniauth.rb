99d71652099079c6db240d261adc4ed6d7fc7aa0
Jeremy Schuurmans <schuurm@ns.codes>
2019-08-28 11:15:15 -0700

    change omniauth configuration to use twitter, change route

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  if Rails.env.development?
-    provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
-  else
-    provider :facebook, ENV['FACEBOOK_KEY_PRODUCTION'], ENV['FACEBOOK_SECRET_PRODUCTION']
-  end
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 end
