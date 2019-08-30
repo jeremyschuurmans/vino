d357a15be78e5ea4e09311b9c37a62a1a745952b
Jeremy Schuurmans <schuurm@ns.codes>
2019-08-30 12:58:51 -0700

    change omniauth to twitter

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root                                 'static_pages#home'
-  get    '/signup',                to: 'users#new'
-  post   '/signup',                to: 'users#create'
-  get    '/auth/github/callback',  to: 'sessions#create'
-  get    '/login',                 to: 'sessions#new'
-  post   '/login',                 to: 'sessions#create'
-  delete '/logout',                to: 'sessions#destroy'
+  get    '/signup',                 to: 'users#new'
+  post   '/signup',                 to: 'users#create'
+  get    '/auth/twitter/callback',  to: 'sessions#create'
+  get    '/login',                  to: 'sessions#new'
+  post   '/login',                  to: 'sessions#create'
+  delete '/logout',                 to: 'sessions#destroy'
   resources :users do
     member do
       get :following, :followers
